0679ffeb830290e537ece027f07532b15662f3ec
SergioBrunacci <sergio.a.brunacci@gmail.com>
2018-01-03 20:06:43 -0500

    Create ToDoItem Class

---
+++ ClearStyle/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  ClearStyle
+//
+//  Created by Sergio de Almeida Brunacci on 2018-01-03.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+
+}
